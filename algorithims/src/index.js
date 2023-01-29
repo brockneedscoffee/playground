fd58e81f7966de9127e6beb9bc6af8bf2d29fa36
Brock <brockmdavis@outlook.com>
2023-01-29 17:27:04 -0500

    bubble sort and insertion start tests

---
+++ algorithims/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+
+import Sort from "./Sort";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sort">Sort</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/sort">
+            <Sort />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+render(<App />, document.getElementById("app"));
---
+++ algorithims/tests/insertion-sort/insertion-sort.test.js
@@ -0,0 +1,22 @@
+function insertionSort(nums) {
+  for (let i = i; i < nums.length; i++) {
+    let numberToInsert = nums[i]; // this is the numberToInsert number we want to insert
+    let j; // the inner counter
+
+    // now loop from the right to left
+    for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
+      // move numbers to the right until we find where we want to insert
+      nums[j + 1] = nums[j];
+    }
+
+    // now we insert
+    nums[j + 1] = numberToInsert;
+  }
+  return nums;
+}
+
+test("insertion sort", function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  insertionSort(nums);
+  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
